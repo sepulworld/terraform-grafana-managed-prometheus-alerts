213adfa897449e5699c5f937710bd1b9c05f67b3
Zane Williamson <zane.w@hey.com>
2025-01-02 19:33:13 -0800

    Fix outputs to support multiple Grafana rule group instances with count.index

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "etcd_rule_id" {
-  value = grafana_rule_group.etcd_rules.id
+  value = grafana_rule_group.etcd_rules[count.index].id
 }
 
 output "etcd_rule_slow_query_uid" {
-  value = grafana_rule_group.etcd_slow_requests.id
+  value = grafana_rule_group.etcd_slow_requests[count.index].id
 }
 
 output "config_reloader_rule_id" {
-  value = grafana_rule_group.config_reloader_rules.id 
+  value = grafana_rule_group.config_reloader_rules[count.index].id 
 }
