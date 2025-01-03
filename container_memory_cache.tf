0289bb134764d94e0e7adc6aaaede7ff49ed5939
Zane Williamson <zane.w@hey.com>
2025-01-02 20:20:14 -0800

    Refactor Grafana rules for container memory cache and swap: rename rules, update recording rule structure, and enhance relative time range comments.

--- container_memory_cache.tf
+++ container_memory_cache.tf
@@ -1,80 +1,50 @@
 resource "grafana_rule_group" "k8s_container_memory_cache" {
-  count            = var.container_memory_cache_rules_enabled ? 1 : 0
-  name             = "k8s_container_memory_cache"
+  name             = "k8s.rules.container_memory_cache"
   folder_uid       = grafana_folder.prometheus_alerts.uid
-  interval_seconds = var.alert_interval_seconds 
+  interval_seconds = var.alert_interval_seconds
 
   rule {
-    name      = "ContainerMemoryCache"
-    condition = "C"
+    name      = "ContainerMemoryCacheRecordingRule"
+    condition = "A" # Reference to the query node
 
-    # Prometheus Query
+    # Recording Rule
+    record {
+      from   = "A" # Refers to the data query
+      metric = "node_namespace_pod_container:container_memory_cache"
+    }
+
+    # Data Query
     data {
       ref_id         = "A"
-      datasource_uid = var.datasource_uid 
+      datasource_uid = var.datasource_uid
       model = jsonencode({
         "editorMode"    = "code",
         "expr"          = "container_memory_cache{job=\"kubelet\", metrics_path=\"/metrics/cadvisor\", image!=\"\"} * on (cluster, namespace, pod) group_left(node) topk by (cluster, namespace, pod) (1, max by (cluster, namespace, pod, node) (kube_pod_info{node!=\"\"}))",
         "intervalMs"    = 1000,
         "maxDataPoints" = 43200,
-        "instant"       = true,
         "refId"         = "A"
       })
-
       relative_time_range {
-        from = 300
-        to   = 0
-      }
-    }
-
-    # Threshold Condition
-    data {
-      ref_id         = "C"
-      datasource_uid = "__expr__"
-      model = jsonencode({
-        "conditions" = [
-          {
-            "evaluator" = {
-              "type"   = "gt",
-              "params" = [0]
-            },
-            "operator" = {
-              "type" = "and"
-            },
-            "query"   = { "params" = ["A"] },
-            "reducer" = { "type" = "last" },
-            "type"    = "query"
-          }
-        ],
-        "datasource"  = { "type" = "__expr__", "uid" = "__expr__" },
-        "expression"  = "A",
-        "intervalMs"  = 1000,
-        "maxDataPoints" = 43200,
-        "refId"       = "C",
-        "type"        = "threshold"
-      })
-
-      relative_time_range {
-        from = 300
-        to   = 0
+        from = 300 # Query range: last 300 seconds
+        to   = 0   # Current time
       }
     }
 
     annotations = {
-      description = "Record container memory cache utilization by cluster, namespace, and pod."
+      description = "Record container memory cache usage by cluster, namespace, and pod."
     }
 
     labels = {
-      severity = "info" 
-    }
-
-    notification_settings {
-      contact_point = var.notification_settings.contact_point
-      mute_timings  = var.notification_settings.mute_timings
-      group_by = var.notification_settings.group_by
+      severity = "info"
     }
 
     no_data_state  = "OK"
     exec_err_state = "OK"
+
+    notification_settings {
+      contact_point = var.notification_settings.contact_point
+      group_by      = ["namespace", "pod"]
+      mute_timings  = var.notification_settings.mute_timings
+    }
   }
 }
--- container_memory_swap.tf
+++ container_memory_swap.tf
@@ -1,14 +1,19 @@
 resource "grafana_rule_group" "k8s_container_memory_swap" {
-  count            = var.container_memory_swap_rules_enabled ? 1 : 0
-  name             = "k8s_container_memory_swap"
+  name             = "k8s.rules.container_memory_swap"
   folder_uid       = grafana_folder.prometheus_alerts.uid
-  interval_seconds = var.alert_interval_seconds 
+  interval_seconds = var.alert_interval_seconds
 
   rule {
-    name      = "ContainerMemorySwap"
-    condition = "C"
+    name      = "ContainerMemorySwapRecordingRule"
+    condition = "A" # Reference to the query node
 
-    # Prometheus Query
+    # Recording Rule
+    record {
+      from   = "A" # Refers to the data query
+      metric = "node_namespace_pod_container:container_memory_swap"
+    }
+
+    # Data Query
     data {
       ref_id         = "A"
       datasource_uid = var.datasource_uid
@@ -17,45 +22,11 @@ resource "grafana_rule_group" "k8s_container_memory_swap" {
         "expr"          = "container_memory_swap{job=\"kubelet\", metrics_path=\"/metrics/cadvisor\", image!=\"\"} * on (cluster, namespace, pod) group_left(node) topk by (cluster, namespace, pod) (1, max by (cluster, namespace, pod, node) (kube_pod_info{node!=\"\"}))",
         "intervalMs"    = 1000,
         "maxDataPoints" = 43200,
-        "instant"       = true,
         "refId"         = "A"
       })
-
       relative_time_range {
-        from = 300  # Adjusted to 5 minutes ago
-        to   = 0    # Up to now
-      }
-    }
-
-    data {
-      ref_id         = "C"
-      datasource_uid = "__expr__"
-      model = jsonencode({
-        "conditions" = [
-          {
-            "evaluator" = {
-              "type"   = "gt",
-              "params" = [0]
-            },
-            "operator" = {
-              "type" = "and"
-            },
-            "query"   = { "params" = ["A"] },
-            "reducer" = { "type" = "last" },
-            "type"    = "query"
-          }
-        ],
-        "datasource"  = { "type" = "__expr__", "uid" = "__expr__" },
-        "expression"  = "A",
-        "intervalMs"  = 1000,
-        "maxDataPoints" = 43200,
-        "refId"       = "C",
-        "type"        = "threshold"
-      })
-
-      relative_time_range {
-        from = 300  # Adjusted to 5 minutes ago
-        to   = 0    # Up to now
+        from = 300 # Query range: last 300 seconds
+        to   = 0   # Current time
       }
     }
 
@@ -64,7 +35,7 @@ resource "grafana_rule_group" "k8s_container_memory_swap" {
     }
 
     labels = {
-      severity = "info" 
+      severity = "info"
     }
 
     no_data_state  = "OK"
@@ -72,8 +43,8 @@ resource "grafana_rule_group" "k8s_container_memory_swap" {
 
     notification_settings {
       contact_point = var.notification_settings.contact_point
+      group_by      = ["namespace", "pod"]
       mute_timings  = var.notification_settings.mute_timings
-      group_by = var.notification_settings.group_by
     }
   }
 }
