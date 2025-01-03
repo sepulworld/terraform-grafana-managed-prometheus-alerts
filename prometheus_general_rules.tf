6f846c6dace7008cd0f4ec0a56a26e4ca920c579
Zane Williamson <zane.w@hey.com>
2025-01-02 20:52:19 -0800

    Refactor Grafana alert rules for target status: rename rules, update conditions, enhance alert descriptions, and adjust relative time range to 5 minutes.

--- prometheus_general_rules.tf
+++ prometheus_general_rules.tf
@@ -1,96 +1,146 @@
-resource "grafana_rule_group" "kube_prometheus_general_rules" {
-  name             = "kube_prometheus_general_rules"
+resource "grafana_rule_group" "kube_prometheus_general_alerts" {
+  name             = "kube_prometheus_general_alerts"
   folder_uid       = grafana_folder.prometheus_alerts.uid
   interval_seconds = var.alert_interval_seconds
 
-  # Rule for count:up1
+  # Alert for count:up0 (Targets down)
   rule {
-    name      = "CountUp1RecordingRule"
-    condition = "A" # Reference to the query node
+    name      = "AlertCountUp0"
+    condition = "C"
 
-    # Recording Rule
-    record {
-      from   = "A" # Refers to the data query
-      metric = "count:up1"
-    }
-
-    # Data Query
+    # Query to check for count:up0 (Targets down)
     data {
       ref_id         = "A"
       datasource_uid = var.datasource_uid
       model = jsonencode({
         "editorMode"    = "code",
-        "expr"          = "count without(instance, pod, node) (up == 1)",
+        "expr"          = "count:up0 > 0", # Alert if any target is down
         "intervalMs"    = 1000,
         "maxDataPoints" = 43200,
         "refId"         = "A"
       })
       relative_time_range {
-        from = 300 # Query range: last 300 seconds
+        from = 300 # Query range: last 5 minutes
         to   = 0   # Current time
       }
     }
 
+    # Alert threshold
+    data {
+      ref_id         = "C"
+      datasource_uid = "__expr__"
+      model = jsonencode({
+        "conditions" = [
+          {
+            "evaluator" = {
+              "type"   = "gt",
+              "params" = [0]
+            },
+            "operator" = {
+              "type" = "and"
+            },
+            "query"   = { "params" = ["A"] },
+            "reducer" = { "type" = "last" },
+            "type"    = "query"
+          }
+        ],
+        "datasource"  = { "type" = "__expr__", "uid" = "__expr__" },
+        "expression"  = "A",
+        "intervalMs"  = 1000,
+        "maxDataPoints" = 43200,
+        "refId"       = "C",
+        "type"        = "threshold"
+      })
+      relative_time_range {
+        from = 300
+        to   = 0
+      }
+    }
+
     annotations = {
-      description = "Counts the number of targets that are up."
+      description = "Alert for down targets. Some targets are not reachable."
     }
 
     labels = {
-      severity = "info"
+      severity = "critical"
     }
 
-    no_data_state  = "OK"
-    exec_err_state = "OK"
+    no_data_state  = "Alerting"
+    exec_err_state = "Alerting"
 
     notification_settings {
       contact_point = var.notification_settings.contact_point
-      group_by      = ["namespace", "pod"]
       mute_timings  = var.notification_settings.mute_timings
     }
   }
 
-  # Rule for count:up0
+  # Alert for count:up1 (Targets up)
   rule {
-    name      = "CountUp0RecordingRule"
-    condition = "A" # Reference to the query node
+    name      = "AlertCountUp1"
+    condition = "C"
 
-    # Recording Rule
-    record {
-      from   = "A" # Refers to the data query
-      metric = "count:up0"
-    }
-
-    # Data Query
+    # Query to check for count:up1 (Targets up)
     data {
       ref_id         = "A"
       datasource_uid = var.datasource_uid
       model = jsonencode({
         "editorMode"    = "code",
-        "expr"          = "count without(instance, pod, node) (up == 0)",
+        "expr"          = "count:up1 == 0", # Alert if no targets are up
         "intervalMs"    = 1000,
         "maxDataPoints" = 43200,
         "refId"         = "A"
       })
       relative_time_range {
-        from = 300 # Query range: last 300 seconds
+        from = 300 # Query range: last 5 minutes
         to   = 0   # Current time
       }
     }
 
+    # Alert threshold
+    data {
+      ref_id         = "C"
+      datasource_uid = "__expr__"
+      model = jsonencode({
+        "conditions" = [
+          {
+            "evaluator" = {
+              "type"   = "eq",
+              "params" = [0]
+            },
+            "operator" = {
+              "type" = "and"
+            },
+            "query"   = { "params" = ["A"] },
+            "reducer" = { "type" = "last" },
+            "type"    = "query"
+          }
+        ],
+        "datasource"  = { "type" = "__expr__", "uid" = "__expr__" },
+        "expression"  = "A",
+        "intervalMs"  = 1000,
+        "maxDataPoints" = 43200,
+        "refId"       = "C",
+        "type"        = "threshold"
+      })
+      relative_time_range {
+        from = 300
+        to   = 0
+      }
+    }
+
     annotations = {
-      description = "Counts the number of targets that are down."
+      description = "Alert for no reachable targets. All targets are down."
     }
 
     labels = {
-      severity = "info"
+      severity = "critical"
     }
 
-    no_data_state  = "OK"
-    exec_err_state = "OK"
+    no_data_state  = "Alerting"
+    exec_err_state = "Alerting"
 
     notification_settings {
       contact_point = var.notification_settings.contact_point
-      group_by      = ["namespace", "pod"]
       mute_timings  = var.notification_settings.mute_timings
     }
   }
--- prometheus_node_recording_rules.tf
+++ prometheus_node_recording_rules.tf
@@ -1,30 +1,33 @@
-resource "grafana_rule_group" "kube_prometheus_general_recording_rules" {
-  count            = var.prometheus_general_recording_rules_enabled ? 1 : 0
+resource "grafana_rule_group" "kube_prometheus_general_rules" {
   name             = "kube_prometheus_general_rules"
   folder_uid       = grafana_folder.prometheus_alerts.uid
   interval_seconds = var.alert_interval_seconds
 
+  # Rule for count:up1
   rule {
-    name      = "CountUp1"
-    condition = "A"
+    name      = "CountUp1RecordingRule"
+    condition = "A" # Reference to the query node
 
+    # Recording Rule
+    record {
+      from   = "A" # Refers to the data query
+      metric = "count:up1"
+    }
+
+    # Data Query
     data {
       ref_id         = "A"
       datasource_uid = var.datasource_uid
       model = jsonencode({
         "editorMode"    = "code",
-        "expr"          = <<EOT
-count without(instance, pod, node) (up == 1)
-EOT
+        "expr"          = "count without(instance, pod, node) (up == 1)",
         "intervalMs"    = 1000,
         "maxDataPoints" = 43200,
-        "instant"       = true,
         "refId"         = "A"
       })
-
       relative_time_range {
-        from = 300 # 5 minutes
-        to   = 0
+        from = 300 # Query range: last 300 seconds
+        to   = 0   # Current time
       }
     }
 
@@ -33,37 +36,44 @@ EOT
     }
 
     labels = {
-      record = "count:up1"
+      severity = "info"
     }
+
+    no_data_state  = "OK"
+    exec_err_state = "OK"
 
     notification_settings {
       contact_point = var.notification_settings.contact_point
+      group_by      = ["namespace", "pod"]
       mute_timings  = var.notification_settings.mute_timings
-      group_by = var.notification_settings.group_by
     }
   }
 
+  # Rule for count:up0
   rule {
-    name      = "CountUp0"
-    condition = "A"
+    name      = "CountUp0RecordingRule"
+    condition = "A" # Reference to the query node
 
+    # Recording Rule
+    record {
+      from   = "A" # Refers to the data query
+      metric = "count:up0"
+    }
+
+    # Data Query
     data {
       ref_id         = "A"
       datasource_uid = var.datasource_uid
       model = jsonencode({
         "editorMode"    = "code",
-        "expr"          = <<EOT
-count without(instance, pod, node) (up == 0)
-EOT
+        "expr"          = "count without(instance, pod, node) (up == 0)",
         "intervalMs"    = 1000,
         "maxDataPoints" = 43200,
-        "instant"       = true,
         "refId"         = "A"
       })
-
       relative_time_range {
-        from = 300 # 5 minutes
-        to   = 0
+        from = 300 # Query range: last 300 seconds
+        to   = 0   # Current time
       }
     }
 
@@ -72,170 +82,16 @@ EOT
     }
 
     labels = {
-      record = "count:up0"
+      severity = "info"
     }
+
+    no_data_state  = "OK"
+    exec_err_state = "OK"
 
     notification_settings {
       contact_point = var.notification_settings.contact_point
+      group_by      = ["namespace", "pod"]
       mute_timings  = var.notification_settings.mute_timings
-      group_by = var.notification_settings.group_by
-    }
-  }
-    # Rule 1: Disk Read Bytes Rate
-  rule {
-    name      = "InstanceNodeDiskReadBytesRateSum"
-    condition = "A"
-
-    data {
-      ref_id         = "A"
-      datasource_uid = var.datasource_uid
-      model = jsonencode({
-        "editorMode"    = "code",
-        "expr"          = <<EOT
-sum(rate(node_disk_read_bytes_total[3m])) BY (instance)
-EOT
-        "intervalMs"    = 1000,
-        "maxDataPoints" = 43200,
-        "instant"       = true,
-        "refId"         = "A"
-      })
-
-      relative_time_range {
-        from = 300 # 5 minutes
-        to   = 0
-      }
-    }
-
-    annotations = {
-      description = "Calculates the disk read bytes rate per instance over a 3-minute window."
-    }
-
-    labels = {
-      record = "instance:node_disk_read_bytes:rate:sum"
-    }
-
-    notification_settings {
-      contact_point = var.notification_settings.contact_point
-      mute_timings  = var.notification_settings.mute_timings
-      group_by = var.notification_settings.group_by
-    }
-  }
-
-  # Rule 2: Disk Write Bytes Rate
-  rule {
-    name      = "InstanceNodeDiskWriteBytesRateSum"
-    condition = "A"
-
-    data {
-      ref_id         = "A"
-      datasource_uid = var.datasource_uid
-      model = jsonencode({
-        "editorMode"    = "code",
-        "expr"          = <<EOT
-sum(rate(node_disk_write_bytes_total[3m])) BY (instance)
-EOT
-        "intervalMs"    = 1000,
-        "maxDataPoints" = 43200,
-        "instant"       = true,
-        "refId"         = "A"
-      })
-
-      relative_time_range {
-        from = 300 # 5 minutes
-        to   = 0
-      }
-    }
-
-    annotations = {
-      description = "Calculates the disk write bytes rate per instance over a 3-minute window."
-    }
-
-    labels = {
-      record = "instance:node_disk_write_bytes:rate:sum"
-    }
-
-    notification_settings {
-      contact_point = var.notification_settings.contact_point
-      mute_timings  = var.notification_settings.mute_timings
-      group_by = var.notification_settings.group_by
-    }
-  }
-
-  # Rule 3: Memory Available Ratio
-  rule {
-    name      = "InstanceNodeMemoryAvailableRatio"
-    condition = "A"
-
-    data {
-      ref_id         = "A"
-      datasource_uid = var.datasource_uid
-      model = jsonencode({
-        "editorMode"    = "code",
-        "expr"          = <<EOT
-avg(node_memory_MemAvailable_bytes / node_memory_MemTotal_bytes) BY (instance)
-EOT
-        "intervalMs"    = 1000,
-        "maxDataPoints" = 43200,
-        "instant"       = true,
-        "refId"         = "A"
-      })
-
-      relative_time_range {
-        from = 300 # 5 minutes
-        to   = 0
-      }
-    }
-
-    annotations = {
-      description = "Calculates the ratio of available memory to total memory per instance."
-    }
-
-    labels = {
-      record = "instance:node_memory:available_ratio"
-    }
-
-    notification_settings {
-      contact_point = var.notification_settings.contact_point
-      mute_timings  = var.notification_settings.mute_timings
-      group_by = var.notification_settings.group_by
-    }
-  }
-    rule {
-    name      = "ClusterNodeCpuRatio"
-    condition = "A"
-
-    data {
-      ref_id         = "A"
-      datasource_uid = var.datasource_uid
-      model = jsonencode({
-        "editorMode"    = "code",
-        "expr"          = <<EOT
-cluster:node_cpu:sum_rate5m / count(sum(node_cpu_seconds_total) BY (instance, cpu))
-EOT
-        "intervalMs"    = 1000,
-        "maxDataPoints" = 43200,
-        "instant"       = true,
-        "refId"         = "A"
-      })
-
-      relative_time_range {
-        from = 300 # 5 minutes
-        to   = 0
-      }
-    }
-
-    annotations = {
-      description = "Calculates the CPU usage ratio for the cluster by dividing the sum of CPU rates by the count of CPUs across instances."
-    }
-
-    labels = {
-      record = "cluster:node_cpu:ratio"
-    }
-
-    notification_settings {
-      contact_point = var.notification_settings.contact_point
-      mute_timings  = var.notification_settings.mute_timings
-      group_by = var.notification_settings.group_by
     }
   }
 }
