27036a4c26c3e01c8063faca1e74ac18d5a6ca2d
Zane Williamson <zane.w@hey.com>
2025-01-02 17:18:43 -0800

    Rename Grafana rule group outputs for clarity and consistency

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "etcd_rule_id" {
-  value = grafana_rule_group.etcd.id 
+  value = grafana_rule_group.etcd_rules.id
 }
 
 output "etcd_rule_slow_query_uid" {
-  value = grafana_rule_group.etcd_slow_query.id 
+  value = grafana_rule_group.etcd_slow_requests.id
 }
 
 output "config_reloader_rule_id" {
