0d7985ad553f5f06d6e3f85d1ebcdc93fa61ffc8
Zane Williamson <zane.w@hey.com>
2025-01-04 12:33:16 -0800

    Add node filesystem space alerts for critical and warning thresholds

--- k8s_node_exporter_alerts.tf
+++ k8s_node_exporter_alerts.tf
@@ -1,4 +1,4 @@
-resource "grafana_rule_group" "node_filesystem_space_filling_up" {
+resource "grafana_rule_group" "node_exporter_alerts"  {
   count           = var.node_filesystem_space_filling_up_rules_enabled ? 1 : 0
   name             = "node_filesystem_space_filling_up"
   folder_uid       = grafana_folder.prometheus_alerts.uid
@@ -96,6 +96,100 @@ EOT
     no_data_state  = "OK"
     exec_err_state = "OK"
     for            = "1h"
+
+    notification_settings {
+      contact_point = var.notification_settings.contact_point
+      group_by      = ["device", "mountpoint", "instance"]
+      mute_timings  = var.notification_settings.mute_timings
+    }
+  }
+  rule {
+    name      = "NodeFilesystemAlmostOutOfSpace5Percent"
+    condition = "A"
+
+    data {
+      ref_id         = "A"
+      datasource_uid = var.datasource_uid
+      model = jsonencode({
+        "editorMode"    = "code",
+        "expr"          = <<EOT
+(
+  node_filesystem_avail_bytes{job="node-exporter",fstype!="",mountpoint!=""} / node_filesystem_size_bytes{job="node-exporter",fstype!="",mountpoint!=""} * 100 < 5
+and
+  node_filesystem_readonly{job="node-exporter",fstype!="",mountpoint!=""} == 0
+)
+EOT
+        "intervalMs"    = 1000,
+        "maxDataPoints" = 43200,
+        "refId"         = "A"
+      })
+      relative_time_range {
+        from = 1800 # Last 30 minutes
+        to   = 0
+      }
+    }
+
+    annotations = {
+      description = "Filesystem on {{ $labels.device }}, mounted on {{ $labels.mountpoint }}, at {{ $labels.instance }} has only {{ printf \"%.2f\" $value }}% available space left."
+      runbook_url = "https://runbooks.prometheus-operator.dev/runbooks/node/nodefilesystemalmostoutofspace"
+      summary     = "Filesystem has less than 5% space left."
+    }
+
+    labels = {
+      severity = "warning"
+    }
+
+    no_data_state  = "OK"
+    exec_err_state = "OK"
+    for            = "30m"
+
+    notification_settings {
+      contact_point = var.notification_settings.contact_point
+      group_by      = ["device", "mountpoint", "instance"]
+      mute_timings  = var.notification_settings.mute_timings
+    }
+  }
+
+  # Alert: Filesystem has less than 3% space left
+  rule {
+    name      = "NodeFilesystemAlmostOutOfSpace3Percent"
+    condition = "A"
+
+    data {
+      ref_id         = "A"
+      datasource_uid = var.datasource_uid
+      model = jsonencode({
+        "editorMode"    = "code",
+        "expr"          = <<EOT
+(
+  node_filesystem_avail_bytes{job="node-exporter",fstype!="",mountpoint!=""} / node_filesystem_size_bytes{job="node-exporter",fstype!="",mountpoint!=""} * 100 < 3
+and
+  node_filesystem_readonly{job="node-exporter",fstype!="",mountpoint!=""} == 0
+)
+EOT
+        "intervalMs"    = 1000,
+        "maxDataPoints" = 43200,
+        "refId"         = "A"
+      })
+      relative_time_range {
+        from = 1800 # Last 30 minutes
+        to   = 0
+      }
+    }
+
+    annotations = {
+      description = "Filesystem on {{ $labels.device }}, mounted on {{ $labels.mountpoint }}, at {{ $labels.instance }} has only {{ printf \"%.2f\" $value }}% available space left."
+      runbook_url = "https://runbooks.prometheus-operator.dev/runbooks/node/nodefilesystemalmostoutofspace"
+      summary     = "Filesystem has less than 3% space left."
+    }
+
+    labels = {
+      severity = "critical"
+    }
+
+    no_data_state  = "OK"
+    exec_err_state = "OK"
+    for            = "30m"
 
     notification_settings {
       contact_point = var.notification_settings.contact_point
