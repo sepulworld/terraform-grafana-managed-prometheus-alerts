9d46dbb498899d026de7fbd25f1a3d23df6f20de
Zane Williamson <zane.w@hey.com>
2025-01-03 20:11:07 -0800

    Add new Prometheus alert rules for deployment generation and replicas mismatch

--- k8s_apps_alerts.tf
+++ k8s_apps_alerts.tf
@@ -38,9 +38,8 @@ EOT
       severity = "warning"
     }
 
+    no_data_state = "OK"
     for = "15m"
-   
-
     notification_settings {
       contact_point = var.notification_settings.contact_point
       group_by      = ["namespace", "pod", "container"]
@@ -88,11 +87,110 @@ EOT
       severity = "warning"
     }
 
+    no_data_state = "OK"
     for = "15m"
 
     notification_settings {
       contact_point = var.notification_settings.contact_point
       group_by      = ["namespace", "pod"]
+      mute_timings  = var.notification_settings.mute_timings
+    }
+  }
+
+    # KubeDeploymentGenerationMismatch Alert
+  rule {
+    name      = "KubeDeploymentGenerationMismatch"
+    condition = "A"
+
+    # Data Query
+    data {
+      ref_id         = "A"
+      datasource_uid = var.datasource_uid
+      model = jsonencode({
+        "editorMode"    = "code",
+        "expr"          = <<EOT
+kube_deployment_status_observed_generation{job="kube-state-metrics", namespace=~".*"}
+  !=
+kube_deployment_metadata_generation{job="kube-state-metrics", namespace=~".*"}
+EOT
+        "intervalMs"    = 1000,
+        "maxDataPoints" = 43200,
+        "refId"         = "A"
+      })
+      relative_time_range {
+        from = 300 # Last 5 minutes
+        to   = 0
+      }
+    }
+
+    annotations = {
+      description = "Deployment generation for {{ $labels.namespace }}/{{ $labels.deployment }} does not match, this indicates that the Deployment has failed but has not been rolled back."
+      runbook_url = "https://runbooks.prometheus-operator.dev/runbooks/kubernetes/kubedeploymentgenerationmismatch"
+      summary     = "Deployment generation mismatch due to possible roll-back"
+    }
+
+    labels = {
+      severity = "warning"
+    }
+
+    no_data_state = "OK"
+    for = "15m"
+
+    notification_settings {
+      contact_point = var.notification_settings.contact_point
+      group_by      = ["namespace", "deployment"]
+      mute_timings  = var.notification_settings.mute_timings
+    }
+  }
+
+  # KubeDeploymentReplicasMismatch Alert
+  rule {
+    name      = "KubeDeploymentReplicasMismatch"
+    condition = "A"
+
+    # Data Query
+    data {
+      ref_id         = "A"
+      datasource_uid = var.datasource_uid
+      model = jsonencode({
+        "editorMode"    = "code",
+        "expr"          = <<EOT
+(
+  kube_deployment_spec_replicas{job="kube-state-metrics", namespace=~".*"}
+    >
+  kube_deployment_status_replicas_available{job="kube-state-metrics", namespace=~".*"}
+) and (
+  changes(kube_deployment_status_replicas_updated{job="kube-state-metrics", namespace=~".*"}[10m])
+    ==
+  0
+)
+EOT
+        "intervalMs"    = 1000,
+        "maxDataPoints" = 43200,
+        "refId"         = "A"
+      })
+      relative_time_range {
+        from = 300 # Last 5 minutes
+        to   = 0
+      }
+    }
+
+    annotations = {
+      description = "Deployment {{ $labels.namespace }}/{{ $labels.deployment }} has not matched the expected number of replicas for longer than 15 minutes."
+      runbook_url = "https://runbooks.prometheus-operator.dev/runbooks/kubernetes/kubedeploymentreplicasmismatch"
+      summary     = "Deployment has not matched the expected number of replicas."
+    }
+
+    labels = {
+      severity = "warning"
+    }
+
+    no_data_state = "OK"
+    for = "15m"
+
+    notification_settings {
+      contact_point = var.notification_settings.contact_point
+      group_by      = ["namespace", "deployment"]
       mute_timings  = var.notification_settings.mute_timings
     }
   }
