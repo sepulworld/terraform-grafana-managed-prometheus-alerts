b4d90e7e99126315f2a63ab846148a7b75bd6185
Zane Williamson <zane.w@hey.com>
2025-01-03 20:59:35 -0800

    Add kube-state-metrics sharding alerts for misconfiguration and missing shards

--- kube_state_metrics_alerts.tf
+++ kube_state_metrics_alerts.tf
@@ -101,4 +101,99 @@ EOT
       mute_timings  = var.notification_settings.mute_timings
     }
   }
+
+    # Alert: KubeStateMetricsShardingMismatch
+  rule {
+    name      = "KubeStateMetricsShardingMismatch"
+    condition = "A"
+
+    # Data Query
+    data {
+      ref_id         = "A"
+      datasource_uid = var.datasource_uid
+      model = jsonencode({
+        "editorMode"    = "code",
+        "expr"          = <<EOT
+stdvar (kube_state_metrics_total_shards{job="kube-state-metrics"}) by (cluster) != 0
+EOT
+        "intervalMs"    = 1000,
+        "maxDataPoints" = 43200,
+        "refId"         = "A"
+      })
+
+      relative_time_range {
+        from = 300 # Last 5 minutes
+        to   = 0   # Current time
+      }
+    }
+
+    annotations = {
+      description = "kube-state-metrics pods are running with different --total-shards configuration, some Kubernetes objects may be exposed multiple times or not exposed at all."
+      runbook_url = "https://runbooks.prometheus-operator.dev/runbooks/kube-state-metrics/kubestatemetricsshardingmismatch"
+      summary     = "kube-state-metrics sharding is misconfigured."
+    }
+
+    labels = {
+      severity = "critical"
+    }
+
+    no_data_state  = "OK"
+    exec_err_state = "OK"
+    for           = "15m"
+
+    notification_settings {
+      contact_point = var.notification_settings.contact_point
+      group_by      = ["cluster"]
+      mute_timings  = var.notification_settings.mute_timings
+    }
+  }
+
+  # Alert: KubeStateMetricsShardsMissing
+  rule {
+    name      = "KubeStateMetricsShardsMissing"
+    condition = "A"
+
+    # Data Query
+    data {
+      ref_id         = "A"
+      datasource_uid = var.datasource_uid
+      model = jsonencode({
+        "editorMode"    = "code",
+        "expr"          = <<EOT
+2^max(kube_state_metrics_total_shards{job="kube-state-metrics"}) by (cluster) - 1
+  -
+sum( 2 ^ max by (cluster, shard_ordinal) (kube_state_metrics_shard_ordinal{job="kube-state-metrics"}) ) by (cluster)
+!= 0
+EOT
+        "intervalMs"    = 1000,
+        "maxDataPoints" = 43200,
+        "refId"         = "A"
+      })
+
+      relative_time_range {
+        from = 300 # Last 5 minutes
+        to   = 0   # Current time
+      }
+    }
+
+    annotations = {
+      description = "kube-state-metrics shards are missing, some Kubernetes objects are not being exposed."
+      runbook_url = "https://runbooks.prometheus-operator.dev/runbooks/kube-state-metrics/kubestatemetricsshardsmissing"
+      summary     = "kube-state-metrics shards are missing."
+    }
+
+    labels = {
+      severity = "critical"
+    }
+
+    no_data_state  = "OK"
+    exec_err_state = "OK"
+    for           = "15m"
+
+    notification_settings {
+      contact_point = var.notification_settings.contact_point
+      group_by      = ["cluster"]
+      mute_timings  = var.notification_settings.mute_timings
+    }
+  }
 }
