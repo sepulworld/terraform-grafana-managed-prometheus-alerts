6031c0b3493cb70255f0f7d8d89f3b36f3da0b03
Zane Williamson <zane.w@hey.com>
2025-01-06 16:36:51 -0800

    Remove unnecessary grouping in kube_pod_container_status_waiting_reason alert query for improved clarity

--- k8s_apps_alerts.tf
+++ k8s_apps_alerts.tf
@@ -464,7 +464,7 @@ EOT
       model = jsonencode({
         "editorMode"    = "code",
         "expr"          = <<EOT
-sum by (namespace, pod, container, cluster) (kube_pod_container_status_waiting_reason{job="kube-state-metrics", namespace=~".*"}) > 0
+kube_pod_container_status_waiting_reason{job="kube-state-metrics", namespace=~".*"} > 0
 EOT
         "intervalMs"    = 1000,
         "maxDataPoints" = 43200,
